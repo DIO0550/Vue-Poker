84025451ee691784dd91ed1c262fff2c73afd72f
DIO0550 <diot12345@yahoo.co.jp>
2020-03-28 19:05:21 +0900

    見た目変更

--- src/frontend/pages/index.vue
+++ src/frontend/pages/index.vue
@@ -1,7 +1,11 @@
 <template>
   <div class="container">
-    <div>ログインID</div>
-    <div>パスワード</div>
+    <div class="login-block">
+      <h1 class="title">Vue-Poker</h1>
+      <div>ログインID</div>
+      <div>パスワード</div>
+      <button>ログイン</button>
+    </div>
   </div>
 </template>
 
@@ -10,4 +14,30 @@ import Vue from 'vue'
 
 export default Vue.extend({})
 </script>
-<style lang="sass" scoped></style>
+<style lang="sass" scoped>
+.container
+  display: flex
+  flex-direction: column
+  justify-content: center
+  align-items: center
+
+  height: 500px
+
+.title
+  color: white
+
+.login-block
+  margin-top: 30px
+  width: 760px
+  display: flex
+  flex-direction: column
+  justify-content: center
+  align-items: center
+  border: solid 5px black
+  box-shadow: 10px 5px 5px rgba(200, 200, 200, 1.0);
+</style>
+
+<style lang="sass">
+body
+    background-color: rgb(144, 238, 144)
+</style>
