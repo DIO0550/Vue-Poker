198d8d95bf4ed9aaa8e0acec8d0a30d86fe71319
DIO0550 <diot12345@yahoo.co.jp>
2020-03-30 22:42:39 +0900

    ボタンの大きさなどを調整。

--- src/frontend/pages/index.vue
+++ src/frontend/pages/index.vue
@@ -1,10 +1,10 @@
 <template>
   <div class="container">
+    <h1 class="title">Vue-Poker</h1>
     <div class="login-block">
-      <h1 class="title">Vue-Poker</h1>
-      <div>ログインID</div>
-      <div>パスワード</div>
-      <button>ログイン</button>
+      <input class="login-field" type="text" placeholder="ログインID" />
+      <input class="password-field" type="password" placeholder="パスワード" />
+      <button class="login-button">ログイン</button>
     </div>
   </div>
 </template>
@@ -15,6 +15,9 @@ import Vue from 'vue'
 export default Vue.extend({})
 </script>
 <style lang="sass" scoped>
+$input-width: 300px
+$input-height: 40px
+
 .container
   display: flex
   flex-direction: column
@@ -35,6 +38,38 @@ export default Vue.extend({})
   align-items: center
   border: solid 5px black
   box-shadow: 10px 5px 5px rgba(200, 200, 200, 1.0);
+
+.login-field
+  margin-top: 10px
+  border: 1px solid black
+  width: $input-width
+  height: $input-height
+  font-size: 25px
+
+.login-field:focus
+  border: 3px solid blue
+  width: $input-width
+  height: $input-height
+
+.password-field
+  margin-top: 10px
+  border: 1px solid black
+  width: $input-width
+  height: $input-height
+  font-size: 25px
+
+.password-field:focus
+  border: 3px solid blue
+  width: $input-width
+  height: $input-height
+
+.login-button
+  width: $input-width
+  height: $input-height
+  margin-top: 10px
+  margin-bottom: 10px
+  background-color: aqua
+  font-size: 25px
 </style>
 
 <style lang="sass">
