20827165bb4e6571bcc4e7bcab62624652bda617
DIO0550 <diot12345@yahoo.co.jp>
2020-04-07 21:52:21 +0900

    パスワードなどの入力欄を追加。パスワードが違う場合にメッセージを表示する処理を追加。

--- src/frontend/pages/register.vue
+++ src/frontend/pages/register.vue
@@ -1,10 +1,40 @@
 <template>
-  <div class="container"></div>
+  <div class="container">
+    <input class="login-field" type="text" placeholder="ログインID" />
+    <input
+      v-model="password"
+      class="password-field"
+      type="password"
+      placeholder="パスワード"
+    />
+    <input
+      v-model="reEnteredPassword"
+      class="password-field"
+      type="password"
+      placeholder="パスワード再入力"
+    />
+    <div v-if="!isSameTwoPassword">パスワードが違います。</div>
+  </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
 
-export default Vue.extend({})
+export default Vue.extend({
+  data() {
+    return {
+      password: '',
+      reEnteredPassword: ''
+    }
+  },
+  computed: {
+    /**
+     * 入力したパスワードが同じかを返す
+     */
+    isSameTwoPassword(): boolean {
+      return this.password === this.reEnteredPassword
+    }
+  }
+})
 </script>
 <style lang="sass" scoped></style>
