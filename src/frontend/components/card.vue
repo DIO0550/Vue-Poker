4cd5f7f3d8f8c350b16d7cf9704b4a7cfceb7fd7
DIO0550 <diot12345@yahoo.co.jp>
2020-04-26 18:24:25 +0900

    カードとゲームのコンポーネント作成。

---
+++ src/frontend/components/card.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="card">
+    {{ cardMark }}
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+const DPokerLib = require('dpoker-lib')
+export default Vue.extend({
+  props: {
+    cardMark: {
+      type: String,
+      default: '♠︎',
+      required: true
+    },
+    cardNumber: {
+      type: Number,
+      default: 0,
+      required: true
+    }
+  },
+  computed: {
+    /**
+     * カードの色
+     */
+    cardColor() {
+      if (
+        this.cardMark === DPokerLib.card.CARD_MARK.SPADE ||
+        this.cardMark === DPokerLib.card.CARD_MARK.CLUB
+      ) {
+        return 'card-black'
+      }
+
+      return 'card-red'
+    }
+  }
+})
+</script>
+<style lang="sass" scoped>
+.card
+    height: 400px
+    width: 200px
+    border-radius: 10px
+    border: 2px solid
+</style>
---
+++ src/frontend/pages/game.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <card></card>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import Card from '../components/card.vue'
+export default Vue.extend({
+  components: { Card }
+})
+</script>
+
+<style lang="sass" scoped></style>
