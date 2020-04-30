547220b6ef2e8dcb59d3663dbbc46f584a0327df
DIO0550 <diot12345@yahoo.co.jp>
2020-04-30 21:29:41 +0900

    役の倍率用コンポーネントを追加。

---
+++ src/frontend/components/HandMagnificationInfo.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="hand-magnification-block">
+    <div>ロイヤルストレートフラッシュ</div>
+    <div>ファイブカード</div>
+    <div>ストレートフラッシュ</div>
+    <div>フォーカード</div>
+    <div>フルハウス</div>
+    <div>フラッシュ</div>
+    <div>ストレート</div>
+    <div>スリーカード</div>
+    <div>ツーペア</div>
+    <div>ワンペア</div>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({})
+</script>
+<style lang="sass" scoped>
+.hand-magnification-block
+    border-radius: 10px
+    border: 5px solid #770000
+</style>
