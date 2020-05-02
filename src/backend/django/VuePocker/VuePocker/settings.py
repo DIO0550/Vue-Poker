192c14e18efb6f0596570dc20c8679ff7fcc8fe6
DIO0550 <diot12345@yahoo.co.jp>
2020-05-02 19:57:17 +0900

    DBの情報や、タイムゾーンなど変更

--- src/backend/django/VuePocker/VuePocker/settings.py
+++ src/backend/django/VuePocker/VuePocker/settings.py
@@ -75,8 +75,11 @@ WSGI_APPLICATION = 'VuePocker.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'vuepoker',
+        'USER': 'vuepoker',
+        'HOST': 'db',
+        'POST': "3306"
     }
 }
 
@@ -103,9 +106,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ja-JP'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Tokyo'
 
 USE_I18N = True
 
