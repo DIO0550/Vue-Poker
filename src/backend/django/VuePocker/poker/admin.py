68515eff23398568bf7208011c82f3e9d499dc0c
DIO0550 <diot12345@yahoo.co.jp>
2020-05-03 21:01:59 +0900

    カスタムユーザーモデル作成（途中）

--- src/backend/django/VuePocker/poker/admin.py
+++ src/backend/django/VuePocker/poker/admin.py
@@ -1,3 +1,21 @@
-from django.contrib import admin
+import uuid
+from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
 
-# Register your models here.
+
+class PokerUserManager(AbstractBaseUser, PermissionsMixin):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+
+    username_validator = UnicodeUsernameValidator()
+
+    username = models.CharField(
+        _('username'),
+        max_length=150,
+        unique=True,
+        help_text=_('Required. 150 characters or fewer. Letters, digits and @/./+/-/_ only.'),
+        validators=[username_validator],
+        error_messages={
+            'unique': _("A user with that username already exists."),
+        },
+    )
+    userpassword = models.CharField(max_length=50)
