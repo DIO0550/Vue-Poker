89097ce5370d9ace57e4530f917bf8fffbd443dc
DIO0550 <diot12345@yahoo.co.jp>
2020-05-05 16:52:22 +0900

    カスタムユーザー調整。

--- src/backend/django/VuePocker/poker/admin.py
+++ src/backend/django/VuePocker/poker/admin.py
@@ -1,14 +1,44 @@
 import uuid
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseUserManager
+from django.contrib.auth.validators import UnicodeUsernameValidator
+import uuid as uuid_lib
 
 class PokerUserManager(BaseUserManager):
-    def create_user(self, password=None, **extra_fields):
+    use_in_migrations = True
+
+    def _create_user(self, username, password, **extra_fields):
+        """
+        Create and save a user with the given username, email, and password.
+        """
+        if not username:
+            raise ValueError('The given username must be set')
+        username = self.model.normalize_username(username)
+        user = self.model(username=username, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_user(self, username, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', False)
+        extra_fields.setdefault('is_superuser', False)
+        return self._create_user(username, password, **extra_fields)
+
+    def create_superuser(self, username, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must have is_staff=True.')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True.')
+
+        return self._create_user(username, password, **extra_fields)
 
 
 class PokerUser(AbstractBaseUser, PermissionsMixin):
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-
+    uuid = models.UUIDField(default=uuid_lib.uuid4,
+                            primary_key=True, editable=False)
     username_validator = UnicodeUsernameValidator()
 
     username = models.CharField(
@@ -25,10 +55,23 @@ class PokerUser(AbstractBaseUser, PermissionsMixin):
 
     userpassword = models.CharField(max_length=50)
 
+    is_staff = models.BooleanField(
+        _('staff status'),
+        default=False,
+        help_text=_('Designates whether the user can log into this admin site.'),
+    )
+    is_active = models.BooleanField(
+        _('active'),
+        default=True,
+        help_text=_(
+            'Designates whether this user should be treated as active. '
+            'Unselect this instead of deleting accounts.'
+        ),
+    )
+
     USERNAME_FIELD="username"
 
     objects = PokerUserManager()
 
     class Meta:
-        db_table = 'poker_user'
         swappable = 'AUTH_USER_MODEL'
