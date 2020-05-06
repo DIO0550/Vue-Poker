b6756b8091fbc03b46814ed856932b8c4d93ceec
DIO0550 <diot12345@yahoo.co.jp>
2020-05-06 20:29:17 +0900

    ユーザーのmigrationまで実装。

--- src/backend/django/VuePocker/VuePocker/settings.py
+++ src/backend/django/VuePocker/VuePocker/settings.py
@@ -15,7 +15,6 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
@@ -26,7 +25,6 @@ SECRET_KEY = '=h%af!mkw-62f^9%#n)8sl6-eb+s_ykmgr&txbvg1%+3kxzrj5'
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
 
 # Application definition
 
@@ -70,7 +68,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'VuePocker.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
@@ -79,11 +76,11 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'vuepoker',
         'USER': 'vuepoker',
+        'PASSWORD': 'vuepoker',
         'HOST': 'db',
         'POST': "3306",
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
@@ -103,7 +100,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/3.0/topics/i18n/
 
@@ -117,11 +113,9 @@ USE_L10N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
 
-
-AUTH_USER_MODEL = 'poker.CustomUser'
+AUTH_USER_MODEL = 'poker.PokerUser'
