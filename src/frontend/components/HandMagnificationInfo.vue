2a2c67e429e5127b4bcdf9dbe1d8fcec5b14424f
DIO0550 <diot12345@yahoo.co.jp>
2020-05-08 21:27:21 +0900

    見た目調整。

--- src/frontend/components/HandMagnificationInfo.vue
+++ src/frontend/components/HandMagnificationInfo.vue
@@ -1,18 +1,56 @@
 <template>
   <div class="hand-magnification-block">
-    <div>
-      <div class="hand">ロイヤルストレートフラッシュ ×1000</div>
-      <div class="hand">ファイブカード ×100</div>
-      <div class="hand">ストレートフラッシュ ×50</div>
-      <div class="hand">フォーカード ×25</div>
-      <div class="hand">フルハウス ×10</div>
+    <div class="hand-manage-list">
+      <table>
+        <tbody>
+          <tr>
+            <td class="hand">ロイヤルストレートフラッシュ</td>
+            <td class="hand">×1000</td>
+          </tr>
+          <tr>
+            <td class="hand">ファイブカード</td>
+            <td class="hand">×50</td>
+          </tr>
+          <tr>
+            <td class="hand">ストレートフラッシュ</td>
+            <td class="hand">×50</td>
+          </tr>
+          <tr>
+            <td class="hand">フォーカード</td>
+            <td class="hand">×25</td>
+          </tr>
+          <tr>
+            <td class="hand">フルハウス</td>
+            <td class="hand">×10</td>
+          </tr>
+        </tbody>
+      </table>
     </div>
-    <div>
-      <div class="hand">フラッシュ ×8</div>
-      <div class="hand">ストレート ×4</div>
-      <div class="hand">スリーカード ×3</div>
-      <div class="hand">ツーペア ×2</div>
-      <div class="hand">ワンペア ×1</div>
+    <div class="hand-manage-list">
+      <table>
+        <tbody>
+          <tr>
+            <td class="hand">フラッシュ</td>
+            <td class="hand">×8</td>
+          </tr>
+          <tr>
+            <td class="hand">ストレート</td>
+            <td class="hand">×4</td>
+          </tr>
+          <tr>
+            <td class="hand">スリーカード</td>
+            <td class="hand">×3</td>
+          </tr>
+          <tr>
+            <td class="hand">ツーペア</td>
+            <td class="hand">×2</td>
+          </tr>
+          <tr>
+            <td class="hand">ワンペア</td>
+            <td class="hand">×1</td>
+          </tr>
+        </tbody>
+      </table>
     </div>
   </div>
 </template>
@@ -27,9 +65,14 @@ export default Vue.extend({})
     border: inset 20px #336600
     background-color: #336666
     display: flex
+    width: auto
 
 .hand
   font-size: 20px
   text-align: center
   color: white
+
+.hand-manage-list
+  width: 500px
+  display: flex
 </style>
